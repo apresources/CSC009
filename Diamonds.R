166ac45854f55282c7db7b597fafadb0aa11a4a0
Andrew Peters <andrew.peters@energybrokersolutions.com>
2019-03-27 15:07:19 +0000

    Finish 1

--- Diamonds.R
+++ Diamonds.R
@@ -3,10 +3,8 @@ library(tidyverse)
 data(diamonds)
 summary(diamonds)
 
-diamonds %>% filter(x < mean(x)) %>%  arrange(desc(price)) %>% head(n = 10) %>%
-  select(clarity, price) %>% group_by(clarity, median(price)) %>% select(group_cols())
+diamonds %>% filter(x < mean(x))
+diamonds %>% arrange(desc(price)) %>% head(n = 10)
 
+diamonds %>% group_by(clarity) %>% summarise(median_price = median(price))
 
-tibble(head(sort(diamonds$price,decreasing=TRUE), n = 10))
-
-subset(diamonds, x < mean(x))
