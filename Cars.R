232ee2db94501c97fcaf768e6074df8d385fefa6
Andrew Peters <andrew.peters@energybrokersolutions.com>
2019-03-27 16:03:58 +0000

    More changes

--- Cars.R
+++ Cars.R
@@ -3,5 +3,10 @@ library(tidyverse)
 data(cars)
 summary(cars)
 
+cars
+
 # http://r-statistics.co/Linear-Regression.html
 
+
+linearMod <- lm(dist ~ speed, data=cars)  # build linear regression model on full data
+print(linearMod)
--- Diamonds.R
+++ Diamonds.R
@@ -4,6 +4,7 @@ data(diamonds)
 summary(diamonds)
 
 diamonds %>% filter(x < mean(x))
+
 diamonds %>% arrange(desc(price)) %>% head(n = 10)
 
 diamonds %>% group_by(clarity) %>% summarise(median_price = median(price))
